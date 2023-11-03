912c81f5ef52b71a8f466a872cf265c95ef7e87a
Goryachih <bogdanrapira@gmail.com>
2023-11-03 10:57:45 +0200

    full auth create category

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as argone2 from 'argon2'
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { User } from 'src/user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor( @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtServise:JwtService,
+  ){}
+  async create(createUserDto: CreateUserDto) {
+    const existUser = await this.userRepository.findOne({
+      where:{
+        email: createUserDto.email,
+      }
+    })
+    if(existUser) throw new BadRequestException('This email alredy exits ')
+
+    const user = await this.userRepository.save({
+      email: createUserDto.email,
+      password: await argone2.hash(createUserDto.password),
+    })
+    const token = this.jwtServise.sign({email:createUserDto.email})
+    return {user , token}
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(email: string){
+    return await this.userRepository.findOne(  
+    {
+        where:{email},
+         
+    }
+    )
   }
 }
